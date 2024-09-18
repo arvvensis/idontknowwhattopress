ff0c472e818c5033665d8d7404a5adfced804d4e
arvensis <arvvensis@gmail.com>
2024-09-18 17:03:36 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,17 @@
-$ ssh-keygen
+#include <iostream>
+using namespace std;
+int main(){
+
+  cout << "enter a name of the best lazy cat in the world" << '\n';
+  string garfield;
+  cin >> garfield;
+  if(garfield = "Garfield"){
+    cout << "you're so fucking right babeeee" << endl;
+  }
+  else {
+    cout << "you're dumb, that's not right!!!!!!!!!!!!!!!!!!" << endl;
+  }
+
+  
+  return 0;
+}
