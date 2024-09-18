aa0d4046fd2226ecae688dcdabef111726cf6c26
arvensis <arvvensis@gmail.com>
2024-09-18 17:26:08 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 int main(){
 
-  cout << "enter a name of the best lazy cat in the world" << '\n';
-  string garfield;
-  cin >> garfield;
-  if(garfield = "Garfield"){
-    cout << "you're so fucking right babeeee" << endl;
-  }
-  else {
-    cout << "you're dumb, that's not right!!!!!!!!!!!!!!!!!!" << endl;
-  }
-
-  
-  return 0;
+    cout << "enter a name of the best lazy cat in the world" << '\n';
+    char g;
+    cin >> g;
+    if ('garfield' == g)
+    {
+        cout << "you're so fucking right babeeee" << endl;
+    }
+    else {
+        cout << "you're dumb, that's not right!!!!!!!!!!!!!!!!!!" << endl;
+    }
+    return 0;
 }
